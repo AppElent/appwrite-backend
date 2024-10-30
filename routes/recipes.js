dce89fde6c6c9bb7c743309d9b641704ddc228f1
AppElent <ericjansen@live.nl>
2024-10-30 12:01:04 +0100

    error handling

--- routes/recipes.js
+++ routes/recipes.js
@@ -3,12 +3,12 @@ const router = new AppExpress.Router();
 
 import getRecipeData from "../services/RecipeDataService.js";
 
-const getRecipeInfo = (req, res) => {
+const getRecipeInfo = async (req, res) => {
   try {
     const url = req.query.url;
-    getRecipeData(url).then((recipeData) => {
-      res.json(recipeData);
-    });
+    if (!url) throw new Error("No URL provided");
+    const recipeData = await getRecipeData(url);
+    res.json(recipeData);
   } catch (e) {
     console.log(e);
     res.json({ error: e });
